576e0829dfd198ab6f678b0a24cb573b631ba2f2
Meyyaou <104941102+Meyyaou@users.noreply.github.com>
2024-06-18 14:47:26 +0100

    Update rpg-text-game.py

--- rpg-text-game.py
+++ rpg-text-game.py
@@ -4,16 +4,35 @@ class Perso():
     
     def __init__(self, name, role, health, strength, inventory):
         print("hello new gamer!")
+        name=input("choose your name: ")
         self.name=name
-        self.role=role
+        print("your name is :", self.name)
+        role= input("choose the role you want to play during this adventure: poet, sorcerer or knight? ")
+        if role=="knight" or role=="poet" or role=="sorcerer":
+            self.role=role 
+            print("you chose to be :", self.role)
+        else:
+            print("there is no place for a", role, "in our world. come back in another garment", self.name)
         self.health=100
         self.strength=10
         self.inventory=None
+        print("your health is initialized to", self.health, "strength to", self.strength, "and in your inventory, you have:", self.inventory)
+        print("are you ready to play? let's start...")
+        
     
-    
-    def __str__():
-        pass
-    
+    def __str__(self):
+        #might add an icon ascii with p/k/s for role at right
+        print("#"*40)
+        print("# Your name:", self.name, "#".rjust(23-len(self.name)))  
+        print("# You are :", self.role, "#".rjust(23-len(self.role)))
+        print("# You have :", self.health, "points of health", "#".rjust(6))
+        print("# You have :", self.strength, "points of strength", "#".rjust(5))
+        if self.inventory is None:
+            print("# You have nothing in your inventory", "#".rjust(3))
+        else: 
+            print("# You have :", self.inventory, "in your inventory", "#".rjust(30-len(self.inventory)))
+        print("#"*40)
+        
     def explore(place):
         pass
     
@@ -45,7 +64,8 @@ class Place():
 class Game():
     
     def __init__(self, starting_point, ending_point):
-        self.starting_point=starting_point
+        print("New game")
+        self.starting_point=None
         self.ending_point=None
         
     def __str__():
@@ -58,4 +78,11 @@ class Game():
         pass
     
     
-        
+#game=Game()
+name=""
+role=""
+health= 100
+strength=10
+inventory={}
+person=Perso(name, role, health, strength, inventory)
+print(person)
