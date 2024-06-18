b0b227d499636a271398ed40aa792e7e680e62a6
Meyyaou <104941102+Meyyaou@users.noreply.github.com>
2024-06-18 14:17:27 +0100

    Create rpg-text-game.py

---
+++ rpg-text-game.py
@@ -0,0 +1,61 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+class Perso():
+    
+    def __init__(self, name, role, health, strength, inventory):
+        print("hello new gamer!")
+        self.name=name
+        self.role=role
+        self.health=100
+        self.strength=10
+        self.inventory=None
+    
+    
+    def __str__():
+        pass
+    
+    def explore(place):
+        pass
+    
+    def combat(perso1, perso2):
+        pass
+    
+    def take_treasure(place):
+        pass
+    
+    def go_to(place):
+        pass
+    
+    def put_down_treasure(treasure):
+        pass
+    
+    def check_inventory(inventory):
+        pass
+    
+class Place():
+    def __init__(self, name, enemy, treasure, quest):
+        self.name=name
+        self.enemy=enemy
+        self.treasure=treasure
+        self.quest=quest
+        
+    def __str__():
+        pass
+    
+class Game():
+    
+    def __init__(self, starting_point, ending_point):
+        self.starting_point=starting_point
+        self.ending_point=None
+        
+    def __str__():
+        pass
+
+    def load_game(self):
+        pass
+    
+    def save_game(self):
+        pass
+    
+    
+        
