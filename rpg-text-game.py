5d0a6755f3eb82332f3d204b7f0a6075c93fdb96
Meyyaou <104941102+Meyyaou@users.noreply.github.com>
2024-06-23 17:25:54 +0100

    Update rpg-text-game.py

--- rpg-text-game.py
+++ rpg-text-game.py
@@ -1,4 +1,4 @@
-# Write Python 3 code in this online editor and run it.
+# press Python 3 code in this online editor and run it.
 import random
 
 class Perso:
@@ -59,7 +59,7 @@ class Perso:
     def explore(self, place):
         if place.name == "Exola":
             print("you're exploring Exola, discovering various places from this kingdom, there is an ancient shop that sells weapons for old soldiers, a flower boutique and a farmer's depot.")
-            kc11=input("write\n 1 if you wanna stop by the antique shop to get yourself the greatest weapon\n 2 if you wanna get romantic and buy a flower for your beloved princess\n or 3 if your stomach is gurling")
+            kc11=input("press\n 1 if you wanna stop by the antique shop to get yourself the greatest weapon\n 2 if you wanna get romantic and buy a flower for your beloved princess\n or 3 if your stomach is gurling")
             if kc11=="1":
                 print("you got yourself a nice new knife")
                 #add more complexity TODO
@@ -96,9 +96,8 @@ class Perso:
         pass
     """
     def check_inventory(self,inventory):
-        if self.inventory is not None:
+        if self.inventory:
             print("you have: ", str(self.inventory), "in your inventory")
-        # NEDD TO FIX THIS TODO elif self.inventory  {}:
         else:
             print("Your inventory is empty.")
 
@@ -111,30 +110,39 @@ class Poet(Perso):
         #print("you chose to be a poet")
     
     def search_for_words(self, place):
-        #TDO addd more complexity here too
+        #ToDO addd more complexity here too
         print("you found no words")
 
-    def write(self, words):
-        #todo add more complexity for this words need to be from words [] or else not
-        print("you have these words:", self.words, "\ and you have to write the best louange for the king or else you die")
+    def press(self, words):
+        print("you have these words:", self.words, "\n and you have to press the best louange for the king or else you die")
         print("think wise:")
         poem=input("")
         
         return poem
-    
+            
+    def check_words(self, poem):
+        poem_words=poem.split(" ")
+        for word in poem_words:
+           if word.lower() not in [w.lower() for w in self.words]:
+                print("you used words beyond your power ")
+                return False
+        return True
+        
+        
     def evaluate_poem(self, poem):
-        if poem is None:
-            print("you have not written anything")
+        if poem is None or not self.check_words(poem):
+            val = 0
         else:
-            val=random.randint(1, 100)
-            if val>50:
-                print("by chance, the king loved it")
-                print("you're fine, you're now called Jaskier and you sing for the king")
-                print("your quest is finished and you'll live a happy romantic life")
-            elif val<50:
-                print("you are dead")
-                self.health=0
-                print("you'll write mourning poems about death in the lambs")
+            val = random.randint(1, 100)
+        if val>50:
+            print("by chance, the king loved it")
+            print("you're fine, you're now called Jaskier and you sing for the king")
+            print("your quest is finished and you'll live a happy romantic life")
+        elif val<50:
+            print("the king decides that you'll soon be sentenced to death")
+            print("you are dead")
+            self.health=0
+            print("you'll press mourning poems about death in the lambs")
                 
 class Knight(Perso):
     def __init__(self, attack, strength, power, name=None, health=100, inventory=None):
@@ -142,8 +150,7 @@ class Knight(Perso):
         self.strength= 10
         self.attack=attack
         self.power=10
-        #print("you chose to be a knight")
-        
+
     def find_enemy(self,perso2):
         pass
     
@@ -155,18 +162,29 @@ class Knight(Perso):
                 print("choose the attack to make him lose: 1) for", self.attack[0]["attack"], "\n2) for", self.attack[1]["attack"])
                 attack_type = input("you chose: ")
                 if attack_type == "1":
-                    enemy.health -= self.attack[0]["points"]
+                    damage = self.attack[0]["points"]
+                    enemy.health -= damage
                 elif attack_type == "2":
-                    enemy.health -= self.attack[1]["points"]
-                print("you made him lose:", self.power)
-                #print("your enemy has now decided to attack back! beware")
+                    damage = self.attack[1]["points"]
+                    enemy.health -= damage
+                print("you made him lose:", damage)
+                
             else:
-                #TODO choos erandom une attack !!
-                print("he attacks you using a deadly attack!")
-                self.health -= enemy.power
-                print("you lose ", enemy.power, " health points")
-    
+                attack_type = random.choice(enemy.attack)
+                print("he chose: ", attack_type)
+                damage = attack_type["points"]
+                self.health -= damage
+                print(f"Enemy attacked with {attack_type['attack']} causing {damage} damage.")
+                print(f"Your health is now {self.health}.")
+            if random.random() > 0.5:
+                self.strength += 10
+                print(f"Your strength increased to {self.strength}.")
+            else:
+                enemy.strength += 15
+                print(f"Enemy strength increased to {enemy.strength}.")
+
         if enemy.health <= 0:
+            print("your enemy is dead")
             print("you won !")
             print("the quest you had to finish is now done")
             print("you finish your game with :", self.health, "health points\n", self.power, "power points\nand", self.strength, " strength points")
@@ -254,7 +272,7 @@ while playing:
     print("here you can be anything you want, explore various places, discover new treasures, make potions and live the most vivacious adventures of all time")
     print("*"*70)
     print("first let's see who you are: ")
-    choice=input("write\n1 to check your character card\n2 to quit the game")
+    choice=input("press\n1 to see your character\n2 to quit the game")
     
     if choice == "2":
         print("oh how sad, do you really want to leave us and go?")
@@ -278,9 +296,9 @@ while playing:
             print("Your quest as a knight is to explore the world, to find an enemy and to combat it.")
             print("your quest starts now")
             place=Place("Exola", "Enemio Enim", None, "COMBAT")
-            enemy=Knight("Katana", 20, 20, "Enemio Enim", 100, {"weapon":"Katana"}, )
+            enemy=Knight([{"points": 5, "attack": "punch"}, {"points": 15, "attack": "sword strike"}], 20, 20, "Enemio Enim", 100, {"weapon":"Katana"}, )
             print(place)
-            kc1=input("write\n1 to explore Exola\n2 to check your inventory\n3 to find your enemy\n4 to quit the game")
+            kc1=input("press\n1 to explore Exola\n2 to check your inventory\n3 to find your enemy\n4 to quit the game")
             if kc1=="4":
                 playing=False
             elif kc1=="2":
@@ -299,25 +317,20 @@ while playing:
             print(f"Your quest as a poet is to explore the world, to collect words and create the most beautiful poem of the universe")
             place=Place("Belova", None, "Love", "POEM")
             print(place)
-            pc1= input("now sweet little poet, what do you want to do ? write\n1 to explore Belova\n2 to search for words\n3 to check your inventory\n4 to quit the game")
+            pc1= input("now sweet little poet, what do you want to do ? press\n1 to explore Belova\n2 to search for words\n3 to check your inventory\n4 to quit the game")
             if pc1=="4":
                 playing= False
             elif pc1=="1":
                 person.explore(place)
             elif pc1=="2":
                 person.role.search_for_words(place)
-                pc11=input("do you want to write a poem?\n1 to start\n2to die because of your inexistant muse ")
+                pc11=input("do you want to press a poem?\n1 to start\n2to die because of your inexistant muse ")
                 if pc11=="1":
-                    writing=person.role.write(person.role.words)
+                    writing=person.role.press(person.role.words)
                     print("the king found your poem and wants you to play it for him. NOW")
-                    pc112=input("declaim or die ")
-                    if pc112=="declaim":
-                        print("the king listens intensively...")
-                        if writing is not None:
-                            person.role.evaluate_poem(writing)
-                    elif pc112=="die":
-                        print("Adieu l'ami")
-                        playing=False
+                    print("the king listens intensively...")
+                    if writing is not None:
+                        person.role.evaluate_poem(writing)
                 elif pc11=="2":
                     print("you died miserably...")
                     print("poor little tortured poet")
