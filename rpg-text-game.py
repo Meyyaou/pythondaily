9879422ca7a7cea93b9227ec29ef748503d9a6e2
Meyyaou <104941102+Meyyaou@users.noreply.github.com>
2024-06-24 03:02:12 +0100

    Update rpg-text-game.py

--- rpg-text-game.py
+++ rpg-text-game.py
@@ -11,7 +11,13 @@ class Perso:
         self.name = name
         self.health = health
         if inventory is None:
-            inventory = {}
+            inventory = {
+                "ingredients":[],
+                "trophy": [],
+                "extra":[],
+                "food":[],
+                "weapon":[]
+            }
         self.inventory = inventory
         if role is None:
             role = self.create_perso()
@@ -96,10 +102,15 @@ class Perso:
                 print("Art piece added to your inventory")
             else:
                 print("try again !")
+                
+                
         elif place.name=="Morder":
             print("You explore Mordor, a dangerous place filled with evil creatures and dark landscapes. You must be vigilant at every step.")
-        sc11 = input("Press:\n1 to explore a mysterious cave\n2 to search for rare ingredients in the dark forest\n3 to challenge a powerful creature\n4 to follow the cursed river\n5 to investigate the enchanted waterfall\n6 choose this to meet a friend")
-    
+        sc11 = input("Press:\n1 to explore a mysterious cave\n2 to search for rare ingredients in the dark forest\n3 to challenge a powerful creature\n4 to follow the cursed river\n5 to investigate the enchanted waterfall\n6 choose this to meet a friend\n")
+        if "ingredients" not in self.inventory:
+            self.inventory["ingredients"] = []
+        if "trophy" not in self.inventory:
+            self.inventory["trophy"]=[]
         if sc11 == "1":
             print("You find a cave filled with magic crystals.")
             self.inventory["ingredients"].append("magic crystals")
@@ -116,14 +127,14 @@ class Perso:
             print("Creature trophy added to your inventory")
         elif sc11 == "4":
             print("You follow the mystic path of the river and find:")
-            found_ingredient = random.choice(["phoenix feather", "dragon scale", "rose petals", "nightingale song", "moonlight essence"])
+            found_ingredient = random.choice(["phoenix feather", "dragon scale", "nightingale song", "moonlight essence"])
             self.inventory["ingredients"].append(found_ingredient)
             print(f"{found_ingredient.capitalize()} added to your inventory")
             print("you hear someone whispering 'come back soon...'\n")
 
         elif sc11 == "5":
             print("you venture towards the melodious waterfall, where powerful energies and rare minerals await.")
-            sc12 = input("Press:\n1 to harvest stardust\n2 to look for a bat wing\n3\nto collect some sparkling water\n ")
+            sc12 = input("Press:\n1 to harvest stardust\n2 to look for a bat wing\n3 to collect some sparkling water\n ")
             
             if sc12 == "1":
                 print("You try and harvset some colorful stardust.")
@@ -141,15 +152,19 @@ class Perso:
             else:
                 print("try again wise ol' man.")
         elif sc11=="6":
+            if "ingredients" not in self.inventory:
+                self.inventory["ingredients"] = []
             print("you hear an angelic voice say to your back, Mellon")
             print("\n you turn around, it's your old elf friend, Legolas, he smiles at you\n")
-            print("you say, my dear Mellon, gen suilon, anann le u-gennin")
-            print("he responds, suilad, i have been waiting for you.\n i have something for you, take this,\n he hands you a purse")
+            print("you say, my dear Mellon, gen suilon, anann le u-gennin\n")
+            print("he responds, suilad, i have been waiting for you.\n i have something for you, take this, he hands you a purse")
             print("you take the purse and look what's in it:\n")
-            print("you find a flask of elf tears\n and a newt's eye")
-            print("exactly what I was missing, rim hennaid")
+            print("you find a flask of elf tears, rose petals and a newt's eye")
+            print("exactly what I was missing, rim hennaid, you told him")
+            print("and go back to exploring and discovering more secrets of the world\n")
             self.inventory["ingredients"].append("elf tears")
             self.inventory["ingredients"].append("eye of newt")
+            self.inventory["ingredients"].append("rose petals")
        
         else:
             print("try again wise old man.")
@@ -278,15 +293,36 @@ class Knight(Perso):
 
    
 class Sorcerer(Perso): 
-    #reaaalyy need to gérer le sorcerer ! TODO
-    
+
     def __init__(self, name=None, health=100, inventory=None):
         super().__init__(name, health, inventory, role=self, silent=True)
         self.power=10
         self.tried_potion = False 
 
     def find_ingredients(self):
-        ingredient_options = ["fire flower", "dragon tooth", "teary cloud", "ancient wooden fragment"]
+        print("let's see what our book of spells says")
+        print("to see what ingredients you need for each potion, select the potion you wanna make and then you need to go explore Mordor to collect your missing ingredients\n")
+        potions = [
+            {"name": "immortality potion", "ingredients": ["magic crystal", "phoenix feather", "dragon scale"]},
+            {"name": "filtre d'amour", "ingredients": ["rose petals", "nightingale song", "moonlight essence"]},
+            {"name": "fairy elixir", "ingredients": ["stardust", "elf tears", "sparkling water"]},
+            {"name": "witches brew", "ingredients": ["toadstool", "eye of newt", "bat wing"]}
+        ]
+        for i, potion in enumerate(potions, 1):
+            print(f"{i}) {potion['name']}")
+        try:
+            selected_index = int(input("Select one by entering the corresponding number:\n"))
+            if 1 <= selected_index <= len(potions):
+                selected_potion = potions[selected_index - 1]
+                print(f"You selected {selected_potion['name']}.")
+                print("You'll need the following ingredients:")
+                for ingredient in selected_potion["ingredients"]:
+                    print(f"- {ingredient}")
+            else:
+                print("Invalid selection. Please try again.")
+        except ValueError:
+            print("Invalid input. Please enter a number corresponding to your choice.")
+        """ingredient_options = ["fire flower", "dragon tooth", "teary cloud", "ancient wooden fragment"]
         found = random.choice(ingredient_options)
         print("you found an ingredient:", found)
         ramasse = input("do you want to take it? (yes or no): ")
@@ -297,13 +333,13 @@ class Sorcerer(Perso):
             print("you added", found, "to your inventory.\n")
         else:
             print("you decided not to take the ingredient.\n")
-            
+            """
     def make_new_potion(self):
         if self.tried_potion:
             return False
         
         potions = [
-            {"name": "immortality potion", "ingredients": ["magic crystal", "phoenix feather", "dragon scale"]},
+            {"name": "immortality potion", "ingredients": ["magic crystals", "phoenix feather", "dragon scale"]},
             {"name": "filtre d'amour", "ingredients": ["rose petals", "nightingale song", "moonlight essence"]},
             {"name": "fairy elixir", "ingredients": ["stardust", "elf tears", "sparkling water"]},
             {"name": "witches brew", "ingredients": ["toadstool", "eye of newt", "bat wing"]}
@@ -313,27 +349,29 @@ class Sorcerer(Perso):
         for i, potion in enumerate(potions, 1):
             print(f"{i}) {potion['name']}")
 
-        choice = input("Enter the number of the potion you want to make: ")
+        choice = input("Enter the index of the potion you want to make: ")
         
         try:
             potion_index = int(choice) - 1
             if 0 <= potion_index < len(potions):
                 selected_potion = potions[potion_index]
                 required_ingredients = selected_potion["ingredients"]
-                if all(ingredient in self.inventory.get("ingredients", []) for ingredient in required_ingredients):
+                inventory_ingredients = self.inventory.get("ingredients", [])
+                if all(inventory_ingredients.count(ingredient) >= required_ingredients.count(ingredient) for ingredient in required_ingredients):
                     potion_name = selected_potion["name"]
                     print(f"You successfully created a {potion_name}.")
+                    
                     for ingredient in required_ingredients:
-                        self.inventory["ingredients"].remove(ingredient)
+                        inventory_ingredients.remove(ingredient)
                     
                     if "potions" not in self.inventory:
                         self.inventory["potions"] = []
                     self.inventory["potions"].append(potion_name)
-                    self.tried_potion = True  # Set the flag to True after successfully making a potion
+                    self.tried_potion = True
                     return True
                 else:
                     print("You don't have all the required ingredients.")
-                    self.tried_potion = True  # Set the flag to True even if the attempt failed
+                    self.tried_potion = True 
                     return False
             else:
                 print("Invalid choice.")
@@ -549,15 +587,15 @@ class Game:
         print(f"Your quest as a sorcerer is to explore the world, to discover new ingredients and make the world's most dangerous potion")
         quest_finished = False
         while self.playing and not quest_finished:
-            sc1 = input("oh great sorcerer ! what would enchant your mind today?\n1 to explore Mordor\n2 to search for the missing ingredients for your potion\n3 to create a new secret potion\n4 to check your inventory\n5 to see the map\n6 to quit game\n")
+            sc1 = input("oh great sorcerer ! what would enchant your mind today?\n1 to search for the missing ingredients for your potion\n2 to explore Mordor\n3 to create a new secret potion\n4 to check your inventory\n5 to see the map\n6 to quit game\n")
             if sc1 == "6":
                 print("pouf, he disappeared suddenly")
                 self.playing = False
             elif sc1 == "5":
               print(place_mor)
-            elif sc1 == "1":
-                self.person.explore(place_mor)
             elif sc1 == "2":
+                self.person.explore(place_mor)
+            elif sc1 == "1":
                 self.person.role.find_ingredients()
             elif sc1 == "3":
                 self.person.role.make_new_potion()
